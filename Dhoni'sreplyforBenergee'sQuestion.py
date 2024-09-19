4845b696ab32a5e81a4f2f05aebd695589e892f4
SHAREALIKHAN <154881648+SHAREALIKHAN@users.noreply.github.com>
2024-09-19 12:37:50 +0530

    Create Dhoni'sreplyforBenergee'sQuestion.py

---
+++ Dhoni'sreplyforBenergee'sQuestion.py
@@ -0,0 +1,25 @@
+'''
+2)After Dhoni made it to the pinnacle of Success as Indian Captain, Mr. Banerjee was once invited by the Media to recall his association with Dhoni. Mr. Banerjee quoted one important moment that complemented the traits of Dhoni. It was when he approached Dhoni and asked him if he would play cricket for the school team saying "Will you be a wicketkeeper?" and was taken aback by Dhoni's confident reply "I will if I get a chance". Write a program to display the answers of Dhoni that impressed his master.
+Input and Output Format:
+Refer Sample input and output for formatting specifications.
+All text in bold corresponds to input and the rest corresponds to output.
+Sample Input and Output:
+Banerjee's Question:
+Will you be a wicketkeeper?
+Dhoni's Reply:
+I will if i get a chance.
+For Banerjee's question "Will you be a wicketkeeper?" Dhoni's confident reply was "I will if I get a chance."
+
+'''
+Answer:
+p=input("Banerjee's Question:\n")
+r=input("Dhoni's Reply:\n")
+print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p,r))
+
+OUTPUT:
+Banerjee's Question:
+Will you be a wicketkeeper?
+Dhoni's Reply:
+I will if i get a chance.
+For Banerjee's Question "Will you be a wicketkeeper?" Dhoni's Confident reply was "I will if i get a chance."
+
